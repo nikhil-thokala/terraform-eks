7606b8b51954556535b70dae5d5f06628f207410
nikhilt <nikhil.t1226@gmail.com>
2024-10-20 21:04:32 +0530

    modules and resources created

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+    default     = "us-east-1"
+    description = "aws-region"
+  
+}
+
+variable "vpc_cidr" {
+    default     = "10.10.10.0/16"
+    description = "default CIDR range of vpc"
+}
+
+variable "kubernetes_version" {
+  default     = 1.27
+  description = "kubernetes version"
+}
